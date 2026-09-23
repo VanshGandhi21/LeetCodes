121bc7cd14f17f21296ef6182577cefe42aca913
VanshGandhi21 <vanshgandhi2105@gmail.com>
2026-09-23 18:43:23 +0530

    Unique element. solvede.

---
+++ ArrayQs/Unique_Element1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int ans = 0;
+    int arr[7] = {1,5,2,2,1,8,8};
+    int size;
+    cin >> size;
+
+    for (int i = 0; i<size ; i++){
+        ans = ans^arr[i];
+    }
+    cout << ans;
+    
+
+}
