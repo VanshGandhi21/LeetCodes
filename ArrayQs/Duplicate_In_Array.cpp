ae455093fa4a4c02393867bfa3cb04ca5de108d8
VanshGandhi21 <vanshgandhi2105@gmail.com>
2026-09-24 19:40:07 +0530

    .\Duplicate in Array. solved

---
+++ ArrayQs/Duplicate_In_Array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int ans = 0;
+
+    int arr[6] = {1,2,3,4,5,2};
+
+    for (int i = 0;i < sizeof(arr) / sizeof(arr[0]); i++) {
+
+        ans = ans ^ arr[i];
+    }
+
+    for(int i = 1; i < sizeof(arr) / sizeof(arr[0]); i++) {
+
+        ans = ans^i;
+    }
+    cout << ans;
+}
