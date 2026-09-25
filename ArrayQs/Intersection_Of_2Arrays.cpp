91eecb669849897865c1d82347094a17c77ecfa8
VanshGandhi21 <vanshgandhi2105@gmail.com>
2026-09-25 22:23:50 +0530

    Solving intersection problem

--- ArrayQs/Intersection_Of_2Arrays.cpp
+++ ArrayQs/Intersection_Of_2Arrays.cpp
@@ -1,26 +1,28 @@
 #include<iostream>
+#include<climits>
 using namespace std;
 
 int main(){
 
     int ans = 0;
 
-    int arr1[5] = {1,2,2,3,4,3};
+    int arr1[6] = {1,2,2,3,4,3};
     int arr2[6] = {7,4,2,1,4,3};
 
-    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++){
+    for (int i = 0; i < sizeof(arr1) / sizeof(arr1[0]); i++){
 
         int element = arr1[i];
 
-        for (int j = 0 ; j < sizeof(arr) / sizeof(arr[0]); j++){
-            if(element == arr2[j]);
+        for (int j = 0 ; j < sizeof(arr2) / sizeof(arr2[0]); j++){
+            if(element == arr2[j]){
 
             ans = ans + element;
             arr2[j] = INT_MIN;
             break;
+            }
         }
     }
 
-    cout << 
+    cout << ans;
 
 }
