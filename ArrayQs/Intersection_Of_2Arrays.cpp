10a57f5cadabcd4882e5a6f667771c9521bca544
VanshGandhi21 <vanshgandhi2105@gmail.com>
2026-09-26 10:21:27 +0530

    Intersection of 2 Arrays.

--- ArrayQs/Intersection_Of_2Arrays.cpp
+++ ArrayQs/Intersection_Of_2Arrays.cpp
@@ -1,31 +1,88 @@
 #include<iostream>
-#include<climits>
 using namespace std;
 
 int main(){
 
+    int arr1[6] = {1,2,2,2,3,4};
+    int arr2[4] = {2,2,3,3};
+
+    int i =0 , j= 0;
     int ans = 0;
 
-    int arr1[6] = {1,2,2,3,4,3};
-    int arr2[6] = {7,4,2,1,4,3};
-
-    for (int i = 0; i < sizeof(arr1) / sizeof(arr1[0]); i++){
-
-        int element = arr1[i];
-
-        for (int j = 0 ; j < sizeof(arr2) / sizeof(arr2[0]); j++){
-            if(element == arr2[j]){
-
+    while(i < sizeof(arr1) / sizeof(arr1[0]) && j < sizeof(arr2) / sizeof(arr2[0])){
+        if(arr1[i]==arr2[j]){
+            int element = arr1[i];
             cout << element << " ";
-            arr2[j] = INT_MIN;
-            break;
-            }
+            i++;
+            j++;
+        }
+        else if(arr1[i]<arr2[j]){
+            i++;
+        }
+        else{
+            j++;
         }
     }
-
     return 0;
-
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// #include<climits>
+// using namespace std;
+
+// int main(){
+
+//     int ans = 0;
+
+//     int arr1[6] = {1,2,2,3,4,3};
+//     int arr2[6] = {7,4,2,1,4,3};
+
+//     for (int i = 0; i < sizeof(arr1) / sizeof(arr1[0]); i++){
+
+//         int element = arr1[i];
+
+//         for (int j = 0 ; j < sizeof(arr2) / sizeof(arr2[0]); j++){
+//             if(element == arr2[j]){
+
+//             cout << element << " ";
+//             arr2[j] = INT_MIN;
+//             break;
+//             }
+//         }
+//     }
+
+//     return 0;
+
+// }
+
 // This is not an optimal solution so..
-// I"ll find it soon.
+// This is the basic logic program the bet and optimal solution is on the top.
+
